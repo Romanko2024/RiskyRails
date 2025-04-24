aad59a1423a83d8d9c8a15b272aad21df3fac00d
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-24 18:35:59 +0300

    Entities_folder

---
+++ RiskyRails/GameCode/Entities/Train.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskyRails.GameCode.Entities
+{
+    public abstract class Train
+    {
+        public Vector2 Position { get; protected set; }
+        public TrackSegment CurrentTrack { get; set; } //!!
+        public bool IsActive { get; protected set; } = true;
+
+        public abstract void Update(GameTime gameTime);
+
+        public virtual void HandleSignal(Signal signal) //!!
+        {
+            //базова логіка реакції на сигнал
+        }
+
+        public virtual void HandleCollision()
+        {
+            IsActive = false;
+        }
+    }
+}
