da506944c36c6f89f38f83a63296b47311197b27
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-05-06 20:56:30 +0300

    IsometricConverter.cs

---
+++ RiskyRails/GameCode/Utilities/IsometricConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace RiskyRails.GameCode.Utilities
+{
+    public static class IsometricConverter
+    {
+        //розміри тайлів
+        public const int TileWidth = 64;
+        public const int TileHeight = 32;
+
+        //перетворення грид-координат в ізометричні
+        public static Vector2 GridToIso(Vector2 gridPos)
+        {
+            return new Vector2(
+                (gridPos.X - gridPos.Y) * TileWidth / 2,
+                (gridPos.X + gridPos.Y) * TileHeight / 2
+            );
+        }
+
+        //зворотнє перетворення
+        public static Vector2 IsoToGrid(Vector2 isoPos)
+        {
+            return new Vector2(
+                (isoPos.X / (TileWidth / 2) + isoPos.Y / (TileHeight / 2)) / 2,
+                (isoPos.Y / (TileHeight / 2) - isoPos.X / (TileWidth / 2)) / 2
+            );
+        }
+    }
+}
