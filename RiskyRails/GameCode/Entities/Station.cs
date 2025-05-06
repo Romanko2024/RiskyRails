e36535389f8035d8f967ed40164775f930909890
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-05-06 19:47:56 +0300

    fixed some stuff and guesing why i have this "managers" folder

---
+++ RiskyRails/GameCode/Entities/Station.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskyRails.GameCode.Entities
+{
+    public class Station : TrackSegment
+    {
+        public string Name { get; set; } = "Unnamed Station";
+        public List<Train> SpawnedTrains { get; } = new();
+    }
+}
---
+++ RiskyRails/GameCode/Managers/RailwayManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using RiskyRails.GameCode.Entities;
+
+namespace RiskyRails.GameCode.Managers
+{
+    public class RailwayManager
+    {
+        public List<TrackSegment> Tracks { get; } = new();
+        public List<Station> Stations { get; } = new();
+
+        public void GenerateTestMap()
+        {
+            //створення станцій
+            var station1 = new Station { GridPosition = new Vector2(2, 2), Name = "Central Station" };
+            var station2 = new Station { GridPosition = new Vector2(8, 8), Name = "East Station" };
+
+            //з'єднання станцій
+            ConnectTracks(station1, station2);
+
+            Tracks.AddRange(new[] { station1, station2 });
+            Stations.AddRange(new[] { station1, station2 });
+        }
+
+        private void ConnectTracks(TrackSegment a, TrackSegment b)
+        {
+            a.ConnectedSegments.Add(b);
+            b.ConnectedSegments.Add(a);
+        }
+    }
+}
